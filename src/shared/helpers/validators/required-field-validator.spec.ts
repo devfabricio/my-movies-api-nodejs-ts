6859320c7aec7bda27e11b4b01cac1692906a7c6
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-18 19:53:20 -0300

    test: ensure that required field validator return anything if field is validated

--- src/shared/helpers/validators/required-field-validator.spec.ts
+++ src/shared/helpers/validators/required-field-validator.spec.ts
@@ -5,9 +5,14 @@ const makeSut = (): RequiredFieldValidator => {
 }
 
 describe('Required Field Validator', () => {
-  test('Should return error if validation fails', () => {
+  it('Should return error if validation fails', () => {
     const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new Error(`Missing Param: ${'field'}`))
+  })
+  it('Should not return anything if the field is validated', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'field' })
+    expect(error).toBeFalsy()
   })
 })
