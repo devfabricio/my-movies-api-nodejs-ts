a85791702c1ad4f66620c7d2b657b46537d7f3db
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-18 20:25:58 -0300

    test: ensure that compare fields validator returns anything if fields values are equals

--- src/shared/helpers/validators/compare-fields-validator.spec.ts
+++ src/shared/helpers/validators/compare-fields-validator.spec.ts
@@ -10,4 +10,10 @@ describe('Required Field Validator', () => {
     const error = sut.validate({ field: 'any_value', fieldToCompare: 'invalid_value' })
     expect(error).toEqual(new Error('Invalid param field'))
   })
+
+  it('Should return anything if the values of the fields are equals', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
