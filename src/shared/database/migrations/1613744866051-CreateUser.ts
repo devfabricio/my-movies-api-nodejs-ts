ed6620e11a5e86f712b2c8c6f727b0c91c798200
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-19 13:29:22 -0300

    feat: create user migration and entity

---
+++ src/shared/database/migrations/1613744866051-CreateUser.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUser1613744866051 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [{
+        name: 'id',
+        type: 'varchar',
+        isPrimary: true,
+        generationStrategy: 'uuid',
+        default: 'uuid_generate_v4()'
+      },
+      {
+        name: 'name',
+        type: 'varchar'
+      },
+      {
+        name: 'email',
+        type: 'varchar',
+        isUnique: true
+      },
+      {
+        name: 'password',
+        type: 'varchar'
+      },
+      {
+        name: 'activation',
+        type: 'integer'
+      },
+      {
+        name: 'isAdmin',
+        type: 'boolean'
+      },
+      {
+        name: 'createdAt',
+        type: 'timestamp',
+        default: 'now()'
+      },
+      {
+        name: 'updatedAt',
+        type: 'timestamp',
+        default: 'now()'
+      }]
+    }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
