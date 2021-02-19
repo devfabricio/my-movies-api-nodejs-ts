022041e3a079dd090d81820788fbe1619e6d6fdb
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-19 13:28:50 -0300

    fix: console error if database connection return an error

--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -1,8 +1,8 @@
 import { createConnection } from 'typeorm'
 
 createConnection()
-.then((connection) => {
-  if (connection.isConnected) {
-    console.log('database connected')
-  }
-})
+  .then((connection) => {
+    if (connection.isConnected) {
+      console.log('database connected')
+    }
+  }).catch(error => console.log(error))
