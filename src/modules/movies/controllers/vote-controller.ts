6c3b8fe4fdbd73251ab9d23bc79a055f1b2a7f8e
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 18:52:08 -0300

    feat: create Create Vote controller

---
+++ src/modules/movies/controllers/vote-controller.ts
@@ -0,0 +1,11 @@
+import { ApiController } from '../../../shared/protocols/api-controller'
+import { Request, Response } from 'express'
+import { makeCreateVoteService } from '../factories/vote/create-vote/make-create-vote-service'
+
+export default class VoteController implements ApiController {
+  async create (request: Request, response: Response): Promise<Response> {
+    const createVote = makeCreateVoteService()
+    const res = await createVote.execute({ body: request.body })
+    return response.json(res)
+  }
+}
