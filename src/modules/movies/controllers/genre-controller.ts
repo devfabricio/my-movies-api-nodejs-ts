a9e92295e737f6b1d1426f44b925c359ff3dc826
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 12:21:10 -0300

    feat: create Create method on GenreController

---
+++ src/modules/movies/controllers/genre-controller.ts
@@ -0,0 +1,11 @@
+import { ApiController } from '../../../shared/protocols/api-controller'
+import { Request, Response } from 'express'
+import { makeCreateGenreService } from '../factories/genre/create-genre/make-create-genre-service'
+
+export default class GenreController implements ApiController {
+  async create (request: Request, response: Response): Promise<Response> {
+    const createGenre = makeCreateGenreService()
+    const res = await createGenre.execute({ body: request.body })
+    return response.json(res)
+  }
+}
