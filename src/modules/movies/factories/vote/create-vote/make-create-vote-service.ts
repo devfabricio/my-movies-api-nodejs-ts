3d26a0920da711cd02b63a46871065a83afaf93a
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 18:50:08 -0300

    feat: create Create Vote factories

---
+++ src/modules/movies/factories/vote/create-vote/make-create-vote-service.ts
@@ -0,0 +1,7 @@
+import { makeCreateVoteValitator } from './make-create-vote-valitator'
+import CreateVoteService from '../../../services/vote/create-vote-service'
+
+export const makeCreateVoteService = (): CreateVoteService => {
+  const validator = makeCreateVoteValitator()
+  return new CreateVoteService(validator)
+}
---
+++ src/modules/movies/factories/vote/create-vote/make-create-vote-valitator.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldValidator, ValidatorComposite } from '../../../../../shared/helpers/validators'
+import { Validator } from '../../../../../shared/helpers/validators/protocols/validator'
+
+export const makeCreateVoteValitator = (): ValidatorComposite => {
+  const validators: Validator[] = []
+  for (const field of ['rate', 'movieId', 'userId']) {
+    validators.push(new RequiredFieldValidator(field))
+  }
+  return new ValidatorComposite(validators)
+}
