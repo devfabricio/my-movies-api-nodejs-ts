f42baa720d3dcd94486291134c1c8cf617545929
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 14:50:06 -0300

    feat: creat index method on Movie Controller

--- src/modules/movies/controllers/movie-controller.ts
+++ src/modules/movies/controllers/movie-controller.ts
@@ -1,11 +1,18 @@
 import { ApiController } from '../../../shared/protocols/api-controller'
-import { Request, Response } from 'express'
+import { Request, response, Response } from 'express'
 import { makeCreateMovieService } from '../factories/movie/create-movie/make-create-movie-service'
+import ListMovieService from '../services/movie/list-movie-service'
 
 export default class MovieController implements ApiController {
   async create (request: Request, response: Response): Promise<Response> {
     const createMovie = makeCreateMovieService()
     const res = await createMovie.execute({ body: request.body })
+    return response.json(res)
+  }
+
+  async index (): Promise<Response> {
+    const listMovie = new ListMovieService()
+    const res = await listMovie.execute()
     return response.json(res)
   }
 }
