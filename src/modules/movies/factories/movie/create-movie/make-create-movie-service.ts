fe72c5aa412e19eb9d9e28007e34c1fa4df908f2
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 13:45:14 -0300

    feat: create Create Movie factories

---
+++ src/modules/movies/factories/movie/create-movie/make-create-movie-service.ts
@@ -0,0 +1,7 @@
+import { makeCreateMovieValitator } from './make-create-movie-valitator'
+import CreateMovieService from '../../../services/movie/create-movie-service'
+
+export const makeCreateMovieService = (): CreateMovieService => {
+  const validator = makeCreateMovieValitator()
+  return new CreateMovieService(validator)
+}
