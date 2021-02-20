dae4446d650d9aae4dd35a60fbb0c9f63f0f8640
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 12:41:48 -0300

    feat: create Create Director method on controller

---
+++ src/modules/movies/controllers/director-controller.ts
@@ -0,0 +1,11 @@
+import { ApiController } from '../../../shared/protocols/api-controller'
+import { Request, Response } from 'express'
+import { makeCreateDirectorService } from '../factories/director/create-director/make-create-director-service'
+
+export default class DirectorController implements ApiController {
+  async create (request: Request, response: Response): Promise<Response> {
+    const createDirector = makeCreateDirectorService()
+    const res = await createDirector.execute({ body: request.body })
+    return response.json(res)
+  }
+}
