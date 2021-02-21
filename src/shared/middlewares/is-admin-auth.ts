1bf5ebbca86dd930ef1a409ea8f1eeedea357aa1
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 21:47:11 -0300

    feat: create Admin middleware

---
+++ src/shared/middlewares/is-admin-auth.ts
@@ -0,0 +1,18 @@
+import { verify } from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express'
+import { UnauthorizedError } from '../helpers/errors'
+
+export const isAdminAuth = (request: Request, response: Response, next: NextFunction): void => {
+  const authHeader = request.headers.authorization
+  if (!authHeader) {
+    throw new UnauthorizedError()
+  }
+
+  const [, token] = authHeader.split(' ')
+  try {
+    verify(token, process.env.SECRET_KEY)
+    return next()
+  } catch (error) {
+    throw new UnauthorizedError()
+  }
+}
