b86a516429659c37f74a9b39d34f72defc20ade2
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-21 17:05:30 -0300

    feat: create Update User Activation route

--- src/modules/users/routes/user.routes.ts
+++ src/modules/users/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/user-controller'
+import { UserActivationController } from '../controllers/user-activation-controller'
 
 const userRouter = Router()
 const userController = new UserController()
+const userActivationController = new UserActivationController()
 
 userRouter.post('/', userController.create)
 userRouter.put('/', userController.update)
+userRouter.put('/activation', userActivationController.update)
 
 export default userRouter
