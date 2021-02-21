10f3bf4166839f70fcf4c3b1c63edc130cfb436c
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-21 17:08:42 -0300

    feat: create Update User Role controller

---
+++ src/modules/users/controllers/user-role-controller.ts
@@ -0,0 +1,11 @@
+import { ApiController } from '../../../shared/protocols/api-controller'
+import { Request, Response } from 'express'
+import { makeUpdateUserRoleService } from '../factories/user/make-update-user-role-service'
+
+export class UserRoleController implements ApiController {
+  async update (request: Request, response: Response): Promise<Response> {
+    const updateUserRole = makeUpdateUserRoleService()
+    const res = await updateUserRole.execute({ body: request.body })
+    return response.json(res)
+  }
+}
