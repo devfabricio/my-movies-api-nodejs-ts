1b6f3af1af2c470e2c77071d43819e51fa251247
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 21:18:35 -0300

    feat: create Authentication factories

---
+++ src/modules/users/factories/authentication/make-authentication-service.ts
@@ -0,0 +1,9 @@
+import { AuthenticationService } from '../../services/authentication-service'
+import { makeAuthenticationValidator } from './make-authentication-validator'
+import { PasswordEncrypterAdapter } from '../../../../shared/utils/adapters/password-encrypter-adapter'
+
+export const makeAuthenticationService = (): AuthenticationService => {
+  const validator = makeAuthenticationValidator()
+  const encrypter = new PasswordEncrypterAdapter()
+  return new AuthenticationService(validator, encrypter)
+}
