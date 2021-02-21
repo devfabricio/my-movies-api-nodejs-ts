d9e3042769c57db731d2362147aa4fb878c9dbfc
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-20 21:22:04 -0300

    feat: create Authentication controller

---
+++ src/modules/users/controllers/authentication-controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+import { ApiController } from '../../../shared/protocols/api-controller'
+import { makeAuthenticationService } from '../factories/authentication/make-authentication-service'
+
+export class AuthenticationController implements ApiController {
+  public async create (request: Request, response: Response): Promise<Response> {
+    const authentication = makeAuthenticationService()
+    const res = await authentication.execute({ body: request.body })
+    return response.json(res)
+  }
+}
