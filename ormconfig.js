52e86925a9887650059e77348b6d7952b7c7ceb3
Fabrício Augusto <fabricio.mata@gmail.com>
2021-02-22 18:28:31 -0300

    feat: refactor ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,49 @@
-const prod_env = process.env.APP_ENVIRONMENT === 'prod'
-const build_env = process.env.APP_ENVIRONMENT === 'build'
+const devPath = {
+  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: [
+    './src/shared/infra/database/migrations/*.ts'
+  ],
+  cli: {
+    migrationsDir: './src/shared/infra/database/migrations'
+  }
+}
+
+const buildPath = {
+  entities: ['./dist/modules/**/infra/typeorm/entities/*.js'],
+  migrations: [
+    './dist/shared/infra/database/migrations/*.js'
+  ],
+  cli: {
+    migrationsDir: './dist/shared/infra/database/migrations'
+  }
+}
+
+const prodPath = {
+  ...buildPath,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
+
+const configPath = () => {
+  const app_env = process.env.APP_ENVIRONMENT
+  let path;
+  switch (app_env) {
+    case 'prod':
+      path = prodPath
+      break
+    case 'build':
+      path = buildPath
+      break
+    default:
+      path = devPath
+  }
+  return path
+}
+
 
 module.exports = {
   type: 'postgres',
@@ -8,17 +52,5 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [prod_env || build_env ? './dist/modules/**/infra/typeorm/entities/*.js' : './src/modules/**/infra/typeorm/entities/*.ts'],
-  migrations: [
-    './src/shared/infra/database/migrations/*.ts'
-  ],
-  cli: {
-    migrationsDir: './src/shared/infra/database/migrations'
-  },
-  ssl: prod_env,
-  extra: prod_env ? {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  } : null
+  ...configPath()
 }
