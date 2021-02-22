7718f2406872cfdb8835cb026037261769842777
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-22 11:23:10 -0300

    chore: update gitignore

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+
+export = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: [
+    './src/shared/database/migrations/*.ts'
+  ],
+  cli: {
+    migrationsDir: './src/shared/database/migrations'
+  }
+}
