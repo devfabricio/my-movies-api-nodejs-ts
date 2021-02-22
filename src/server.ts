f6dda9e3d82c3f34b26d4d81c98f8911b02dfd4b
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-22 16:31:56 -0300

    refactor: change call database location

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config'
-import './shared/infra/database'
 import app from './shared/main/config/app'
 
 app.listen(process.env.PORT || 8000, () => {
--- src/shared/main/config/app.ts
+++ src/shared/main/config/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import setupSwagger from '../../doc/swagger'
 import setupRoutes from '../routes'
 import setupMiddlewares from './middlewares'
+import '../../infra/database'
 
 const app = express()
 
